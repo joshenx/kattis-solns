1a47cf997bf422155c40bd4e73c54d95799bb868
Joshen Lim <77193243+joshenx@users.noreply.github.com>
2021-02-12 16:11:52 +0800

    Create carrots.java

---
+++ carrots.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] numbers = input.split(" ");
+        int n = Integer.parseInt(numbers[0]);
+        int p = Integer.parseInt(numbers[1]);
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+        }
+        System.out.println(p);
+    }
+}
